ff9e7e8b7823499d57b29617783b66f17fd36417
Ali Mahmudi <mahmudia330@gmail.com>
2023-11-22 20:10:13 +0700

    keenam

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -63,7 +63,7 @@ class AuthController extends Controller
 
         $request->session()->invalidate();
 
-        return redirect('/');
+        return redirect('/login');
     }
 
     public function profile()
--- resources/views/crud/dash.blade.php
+++ resources/views/crud/dash.blade.php
@@ -183,7 +183,8 @@
                 <div class="mx-auto">
                     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                         <h1 class="h2">View Data</h1>
-                        <a href=""><button type="button" class="btn btn-primary">CRUD</button></a>
+                        <a href="{{ route('logout') }}"><button type="button" class="btn btn-danger">LOGOUT
+                            </button></a>
                     </div>
 
                 </div>
